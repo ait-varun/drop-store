7d66302fdd0ed2f2d36727b13adc85a15e69ad5f
Varun <aitvarunbhatia@gmail.com>
2024-10-02 14:02:57 +0530

    updated

--- src/components/layout/productCard.tsx
+++ src/components/layout/productCard.tsx
@@ -9,6 +9,20 @@ export default function ProductCard({ product }: { product: Product }) {
   const { addToCart, cart, updateQuantity, removeFromCart } = useCart();
   const cartItem = cart.find((item) => item.id === product.id);
 
+  const keyStr =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+
+  const triplet = (e1: number, e2: number, e3: number) =>
+    keyStr.charAt(e1 >> 2) +
+    keyStr.charAt(((e1 & 3) << 4) | (e2 >> 4)) +
+    keyStr.charAt(((e2 & 15) << 2) | (e3 >> 6)) +
+    keyStr.charAt(e3 & 63);
+
+  const rgbDataURL = (r: number, g: number, b: number) =>
+    `data:image/gif;base64,R0lGODlhAQABAPAA${
+      triplet(0, r, g) + triplet(b, 255, 255)
+    }/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`;
+
   return (
     <Card className="w-full">
       <CardContent className="p-4">
@@ -17,6 +31,8 @@ export default function ProductCard({ product }: { product: Product }) {
             src={product.image}
             alt={product.title}
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            placeholder="blur"
+            blurDataURL={rgbDataURL(131, 131, 131)}
             fill
             className="object-cover rounded-md"
           />
